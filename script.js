0b569a78ae9e7e75c6704953edb46f361e6ff27f
Arsh Ilahi <156427743+Arsh249@users.noreply.github.com>
2024-08-17 15:12:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+ function setBackgroundImage() {
+            if (window.innerWidth > window.innerHeight) {
+                // Landscape mode
+                document.body.style.backgroundImage = "url('https://photovideocreative.com/wordpress/wp-content/uploads/2017/11/Paysage-en-orientation-paysage.jpg')";
+            } else {
+                // Portrait mode
+                document.body.style.backgroundImage = "url('https://photovideocreative.com/wordpress/wp-content/uploads/2017/11/Portrait-en-orientation-portrait.jpg')";
+            }
+        }
+
+        // Initial check
+        setBackgroundImage();
+
+        // Add event listener for resize event
+        window.addEventListener('resize', setBackgroundImage);
